869c6e1ce92a26c8d06a77881dfc14ded9808b5d
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-06-27 10:44:17 +0900

    add test

---
+++ iOSLayeredSampleTests/StarColorTests.swift
@@ -0,0 +1,21 @@
+//
+//  StarColorTests.swift
+//  iOSLayeredSampleTests
+//
+//  Created by rnishimu on 2020/06/27.
+//  Copyright © 2020 rnishimu22001. All rights reserved.
+//
+
+import XCTest
+@testable import iOSLayeredSample
+
+final class StarColorTests: XCTestCase {
+    func testStarCount() {
+        XCTContext.runActivity(named: "Star10000以上", block: { _ in
+            XCTAssertEqual(StarColor(with: 10000), .red)
+        })
+        XCTContext.runActivity(named: "Star10000未満", block: { _ in
+            XCTAssertEqual(StarColor(with: 9999), .lightGray)
+        })
+    }
+}
