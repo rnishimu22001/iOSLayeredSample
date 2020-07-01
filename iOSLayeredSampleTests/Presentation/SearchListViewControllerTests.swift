f981ea2d1522fecc6112c8ae0d7f20c960d8858b
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-07-01 23:52:39 +0900

    add assert

--- iOSLayeredSampleTests/Presentation/SearchListViewControllerTests.swift
+++ iOSLayeredSampleTests/Presentation/SearchListViewControllerTests.swift
@@ -21,13 +21,15 @@ final class SearchListViewControllerTest: XCTestCase {
         let target = SearchListViewController()
         target.navigator = navigator
         target.viewModel = viewModel
+        let repositoryName = "test"
         // When
-        let repository = Repository(fullName: "test", description: nil, starCount: 100)
+        let repository = Repository(fullName: repositoryName, description: nil, starCount: 100)
         viewModel.stubbedRepositoryInListResult = RepositoryDisplayData(from: repository)
         target.searchListView(SearchListView(parentView: UIView()),
                               didSelectRepositoryListAt: 0)
         // Then
         XCTAssertEqual(navigator.invokedPushCount, 1, "画面が一回pushされること")
         XCTAssertTrue(navigator.invokedPushParametersList.first!.viewControler is DetailViewController, "詳細画面がpushされること")
+        XCTAssertEqual((navigator.invokedPushParametersList.first!.viewControler as?  DetailViewController)!.repositoryFullName, repositoryName, "Repository名が代入されていること")
     }
 }
